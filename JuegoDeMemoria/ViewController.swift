6d50753b5e5f42cb2d193505991126645b6641ec
Mariona Roca <mariona@mbp-de-mariona.home>
2016-01-01 13:49:06 +0100

    Initial Commit

---
+++ JuegoDeMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuegoDeMemoria
+//
+//  Created by Mariona on 1/1/16.
+//  Copyright © 2016 Mariona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
